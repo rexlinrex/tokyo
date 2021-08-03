dcdd4da3bf12adb6af43602cd2a455564a5c9021
rexlinrex <rexlinrex99@gmail.com>
2021-08-03 14:27:25 +0530

    Password Functionality

---
+++ src/org/test/classSample.java
@@ -0,0 +1,9 @@
+package org.test;
+public class classSample {
+	public static void passWord() {
+		System.out.println("Nirmala@1");
+	}
+	public static void main(String[] args) {
+		passWord();
+	}
+}

806c9a82e9db6dec64adbe29b8165f0364fcd087
rexlinrex <rexlinrex99@gmail.com>
2021-08-03 14:16:47 +0530

    userName fuctionality

---
+++ src/org/test/classSample.java
@@ -0,0 +1,9 @@
+package org.test;
+public class classSample {
+	public static void userName() {
+		System.out.println("rbilave");
+	}
+	public static void main(String[] args) {
+		userName();
+	}
+}
